8cd1858211190e709997bc81f70f2d7b55e9c1f2
KOUSHIK PANUGANTI <kpanuganti@google.com>
2018-04-09 16:49:03 -0700

    Migrated various apps under packages/apps/Car/SystemUpdater   to androidx
    Bug: 76692459
    Test: make SystemUpdater
    Change-Id: I542d5343f830eaecee1ef12a5911871a1c433d65

--- src/com/android/car/systemupdater/SystemUpdaterActivity.java
+++ src/com/android/car/systemupdater/SystemUpdaterActivity.java
@@ -18,10 +18,10 @@ package com.android.car.systemupdater;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.io.File;
 
